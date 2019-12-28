56fa85a150a08c5e7e7065720c606246a6cc57cc
kairosumida <kairosumida@hotmail.com>
2019-12-28 17:21:54 -0300

    Adicionado Rigidbody aos componentes

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+    [SerializeField] private float speed = 20f;
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +14,6 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        transform.Translate(Vector3.forward);
+        transform.Translate(Vector3.forward *Time.deltaTime * speed);
     }
 }
