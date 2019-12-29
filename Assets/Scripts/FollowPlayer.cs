154e9fcd3b449e924d4200be4b225e81b80f06d9
kairosumida <kairosumida@hotmail.com>
2019-12-28 21:19:54 -0300

    OffSetCamera

--- Assets/Scripts/FollowPlayer.cs
+++ Assets/Scripts/FollowPlayer.cs
@@ -5,15 +5,19 @@ using UnityEngine;
 public class FollowPlayer : MonoBehaviour
 {
     public GameObject player;
+    private Vector3 camPosInitial;
     // Start is called before the first frame update
     void Start()
     {
-        
+        camPosInitial = transform.position;
     }
-
+    private void LateUpdate()
+    {
+        transform.position = player.transform.position + camPosInitial;
+    }
     // Update is called once per frame
     void Update()
     {
-        transform.position = player.transform.position;
+        
     }
 }
